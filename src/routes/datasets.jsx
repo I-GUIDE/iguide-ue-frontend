c603a21de28ee7aca2352e8fb5ae7643d1e881ea
Erick Li <ericklizh@gmail.com>
2024-06-03 16:43:22 -0500

    Add notebook and dataset list to the page

--- src/routes/datasets.jsx
+++ src/routes/datasets.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+
+import Stack from '@mui/joy/Stack';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+
+import InfoCard from '../components/InfoCard';
+import Header from '../components/Layout/Header';
+
+import datasetMetadata from '../assets/metadata/dataset-metadata.json';
 
 const Datasets = () => {
     return (
-        <Container maxWidth="xl">
-            <Typography
-                variant="h6"
+        <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            <Box
+                component="main"
                 sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
-                    letterSpacing: '.2rem',
-                    color: 'inherit',
-                    textDecoration: 'none',
+                    height: 'calc(100vh - 55px)', // 55px is the height of the NavBar
+                    display: 'grid',
+                    gridTemplateColumns: { xs: 'auto', md: '100%' },
+                    gridTemplateRows: 'auto 1fr auto',
                 }}
             >
-                There will be a list of datasets
-            </Typography>
-        </Container>
+                <Stack
+                    sx={{
+                        backgroundColor: 'background.surface',
+                        px: { xs: 2, md: 4 },
+                        py: 2,
+                        borderBottom: '1px solid',
+                        borderColor: 'divider',
+                    }}
+                >
+                    <Stack spacing={2} sx={{ px: { xs: 2, md: 4 }, pt: 2, minHeight: 0 }}>
+                    <Header title='Datasets' subtitle='Find your datasets here.' />
+                        {datasetMetadata.map((dataset) => (
+                            <InfoCard
+                                title={dataset.title}
+                                subtitle={dataset.author}
+                                tags={dataset.tags}
+                                contents={dataset.contents} />
+                        ))}
+                    </Stack>
+                </Stack>
+            </Box>
+        </CssVarsProvider>
     )
 }
 
