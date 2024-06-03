4fab15c6e7cee2ce5613271220015c861f2af556
Erick Li <ericklizh@gmail.com>
2024-06-03 08:33:46 -0500

    Create basic framework

---
+++ src/routes/datasets.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Datasets = () => {
+    return (
+        <div>
+            <h1>There will be a list of datasets</h1>
+        </div>
+    )
+}
+
+export default Datasets;
---
+++ src/routes/notebooks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notebooks = () => {
+    return (
+        <div>
+            <h1>There will be a list of notebooks</h1>
+        </div>
+    )
+}
+
+export default Notebooks;
