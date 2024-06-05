915e1e53e396ed5c8c9307d00139373551067a28
Erick Li <ericklizh@gmail.com>
2024-06-05 17:41:47 -0500

    Update dataset page layout

--- src/components/Pages/DatasetPage.jsx
+++ src/components/Pages/DatasetPage.jsx
@@ -2,13 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { Link } from '@mui/joy';
-
-import Header from '../Layout/Header';
+import Stack from '@mui/joy/Stack';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Grid from '@mui/joy/Grid';
+import Typography from '@mui/joy/Typography';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
+import Chip from '@mui/joy/Chip';
+import Button from '@mui/joy/Button';
+import Container from '@mui/joy/Container';
 
 import Datasets from '../../assets/metadata/dataset-metadata.json';
+import './NotebookIFrame.css';
 
 function DatasetPage() {
-    const [text, setText] = useState("");
+    const [title, setTitle] = useState("")
+    const [author, setAuthor] = useState("")
+    const [abstract, setAbstract] = useState("")
+    const [tags, setTags] = useState([])
     const [relatedNotebooks, setRelatedNotebooks] = useState([]);
     const id = useParams().id;
 
@@ -18,34 +32,111 @@ function DatasetPage() {
             var obj = Datasets[i];
             if (obj.id == id) {
                 setRelatedNotebooks(obj['related-notebooks']);
-                setText(obj.title);
+                setTitle(obj.title);
+                setAuthor(obj.author);
+                setAbstract(obj.contents);
+                setTags(obj.tags);
             }
         }
     });
 
     return (
-        <div className="content">
-            <Header title={id} subtitle={text} />
-            <div className="project-text">Related notebook(s):</div>
-            {relatedNotebooks.map((notebook) => (
-                <Link
-                    underline="none"
-                    href={"/notebooks/" + notebook}
-                    sx={{ color: 'text.tertiary' }}
+        <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            <Container maxWidth="xl">
+                <Box
+                    component="main"
+                    sx={{
+                        height: 'calc(100vh - 55px)', // 55px is the height of the NavBar
+                        display: 'grid',
+                        gridTemplateColumns: { xs: 'auto', md: '100%'},
+                        gridTemplateRows: 'auto 1fr auto',
+                    }}
                 >
-                    {notebook}
-                </Link>
-            ))}
-            <div className="project-text">
-                <Link
-                    underline="none"
-                    href="/datasets"
-                    sx={{ color: 'text.tertiary' }}
-                >
-                    Go Back
-                </Link>
-            </div>
-        </div>
+                    <Grid
+                        container
+                        rowSpacing={2}
+                        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+                        sx={{
+                            backgroundColor: 'inherit',
+                            px: { xs: 2, md: 4 },
+                            py: 2,
+                            borderBottom: '1px solid',
+                            borderColor: 'divider',
+                        }}
+                    >
+                        <Grid xs={4}>
+                            <Stack spacing={2} sx={{ px: { xs: 2, md: 4 }, pt: 2, minHeight: 0 }}>
+                                <Typography level="h1">{title}</Typography>
+                                <Typography level="h3" fontSize="xl" sx={{ mb: 0.5 }}>
+                                    Created by {author}
+                                </Typography>
+                                <Typography>
+                                    {abstract}
+                                </Typography>
+                                <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+                                    <Typography
+                                        id="notebook-tags"
+                                        level="h5"
+                                        fontWeight="lg"
+                                        mb={1}
+                                    >
+                                        Tags:
+                                    </Typography>
+                                    {tags.map((tag) => (
+                                        <Chip
+                                            key={tag}
+                                            variant="outlined"
+                                            color="primary"
+                                            size="sm"
+                                            sx={{ pointerEvents: 'none' }}
+                                        >
+                                            {tag}
+                                        </Chip>
+                                    ))}
+                                </Box>
+                                <Typography
+                                    id="notebook-ds"
+                                    level="h5"
+                                    fontWeight="lg"
+                                    mb={1}
+                                >
+                                    Related notebooks:
+                                </Typography>
+                                <List aria-labelledby="decorated-list-demo">
+                                    {relatedNotebooks.map((notebook) => (
+                                        <Link
+                                            underline="none"
+                                            href={"/notebooks/" + notebook}
+                                            sx={{ color: 'text.tertiary' }}
+                                        >
+                                            <ListItem>
+                                                <ListItemButton>
+                                                    {notebook}
+                                                </ListItemButton>
+                                            </ListItem>
+                                        </Link>
+                                    ))}
+                                </List>
+                                <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
+                                    <Button size="sm">
+                                        <Link
+                                            underline="none"
+                                            href="/notebooks"
+                                            sx={{ color: 'inherit' }}
+                                        >
+                                            Go Back
+                                        </Link>
+                                    </Button>
+                                </Box>
+                            </Stack>
+                        </Grid>
+                        <Grid xs={8}>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </CssVarsProvider>
     )
 }
 
