fe0b4b546dda9c4e0a25b2f92547e61dc33b7d4c
Erick Li <ericklizh@gmail.com>
2024-06-12 19:07:01 -0500

    Update notebook page for retrieving from the backend

--- src/utils/DataRetriever.jsx
+++ src/utils/DataRetriever.jsx
@@ -1,22 +1,8 @@
-/*import Datasets from '/src/assets/metadata/dataset-metadata.json';
-import Notebooks from '/src/assets/metadata/notebook-metadata.json';
-
-export function DataRetriever(data_name) {
-    if (data_name == 'datasets') {
-        return Datasets;
-    } else if (data_name == 'notebooks') {
-        return Notebooks;
-    } else {
-        throw new Error('Wrong data_name!');
-    }
-}*/
-
-
 export async function DataRetriever(data_name) {
-  const response = await fetch(`http://localhost:5000/api/resources?data_name=${data_name}`);
-  if (!response.ok) {
-    throw new Error(`Error fetching ${data_name}: ${response.statusText}`);
-  }
-  const data = await response.json();
-  return data;
+    const response = await fetch(`http://149.165.169.173:5000/api/resources?data_name=${data_name}`);
+    if (!response.ok) {
+        throw new Error(`Error fetching ${data_name}: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
 }
