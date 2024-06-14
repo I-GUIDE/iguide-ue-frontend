1cdb0d2ff700ebde529449de7c64c2374ce2d747
Yunfan <exouser@datawithnotebooks-prototype-dev.js2local>
2024-06-14 19:45:32 +0000

    Add code generate js function

--- src/utils/DataRetriever_test.js
+++ src/utils/DataRetriever_test.js
@@ -1,8 +1,11 @@
 export async function DataRetriever(data_name) {
-    const response = await fetch(`http://149.165.169.173:5000/api/resources?data_name=${data_name}`);
+    const response = await fetch(`http://149.165.169.173:3000/api/resources?data_name=${data_name}`);
     if (!response.ok) {
         throw new Error(`Error fetching ${data_name}: ${response.statusText}`);
     }
     const data = await response.json();
     return data;
 }
+DataRetriever('notebook').then(result => {
+    console.log(result); // Print the result to the console
+});
