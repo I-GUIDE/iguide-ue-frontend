92c1f67cfb784ad76f25642064b28044fb833cef
Erick Li <ericklizh@gmail.com>
2024-06-17 15:33:46 -0500

    Create component codesnippet for resource pages

--- src/routes/ResourcePages/DatasetPage.jsx
+++ src/routes/ResourcePages/DatasetPage.jsx
@@ -8,19 +8,17 @@ import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 import Box from '@mui/joy/Box';
 import Grid from '@mui/joy/Grid';
-import Typography from '@mui/joy/Typography';
-import List from '@mui/joy/List';
-import ListItem from '@mui/joy/ListItem';
-import ListItemButton from '@mui/joy/ListItemButton';
-import Chip from '@mui/joy/Chip';
 import Button from '@mui/joy/Button';
 import Container from '@mui/joy/Container';
 import CloudDownloadOutlinedIcon from '@mui/icons-material/CloudDownloadOutlined';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
-import { extractValueFromJSON, printListWithDelimiter } from '../../helpers/helper';
 import { DataRetriever } from '../../utils/DataRetrieval';
 import './NotebookIFrame.css';
+import MainContent from '../../components/ResourcePagesComps/MainContent';
+import CapsuleList from '../../components/ResourcePagesComps/CapsuleList';
+import RelatedResourcesList from '../../components/ResourcePagesComps/RelatedResourcesList';
+import CodeSnippet from '../../components/ResourcePagesComps/CodeSnippet';
 
 function DatasetPage() {
     const [title, setTitle] = useState('');
@@ -31,15 +29,11 @@ function DatasetPage() {
     const [externalLink, setExternalLink] = useState('');
     const [directDownloadLink, setDirectDownloadLink] = useState('');
     const [size, setSize] = useState('');
-    const [downloadInstruction, setDownloadInstruction] = useState('');
     const id = useParams().id;
-    const [notebooks, setNotebooks] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             const Datasets = await DataRetriever('dataset');
-            const Notebooks = await DataRetriever('notebook');
-            setNotebooks(Notebooks);
 
             for (const obj of Datasets) {
                 if (obj.id === id) {
@@ -51,7 +45,6 @@ function DatasetPage() {
                     setExternalLink(obj['external-link']);
                     setDirectDownloadLink(obj['direct-download-link']);
                     setSize(obj.size);
-                    setDownloadInstruction('! wget ' + obj['direct-download-link']);
                     break;
                 }
             }
@@ -85,33 +78,9 @@ function DatasetPage() {
                         }}
                     >
                         <Grid xs={12}>
+                            <MainContent title={title} authors={authors} contents={abstract} />
+                            <CapsuleList title="Tags" items={tags} />
                             <Stack spacing={2} sx={{ px: { xs: 2, md: 4 }, pt: 2, minHeight: 0 }}>
-                                <Typography level="h1">{title}</Typography>
-                                <Typography level="h3" fontSize="xl" sx={{ mb: 0.5 }}>
-                                    Created by {printListWithDelimiter(authors, ',')}
-                                </Typography>
-                                <Typography>
-                                    {abstract}
-                                </Typography>
-                                <Typography
-                                    id="notebook-tags"
-                                    level="h5"
-                                    fontWeight="lg"
-                                    mb={1}
-                                >
-                                    Tags:
-                                </Typography>
-                                {tags.map((tag) => (
-                                    <Chip
-                                        key={tag}
-                                        variant="outlined"
-                                        color="primary"
-                                        size="sm"
-                                        sx={{ pointerEvents: 'none' }}
-                                    >
-                                        {tag}
-                                    </Chip>
-                                ))}
                                 <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
                                     <Button size="sm">
                                         <Link
@@ -140,48 +109,8 @@ function DatasetPage() {
                                         </Link>
                                     </Button>
                                 </Box>
-                                <Box>
-                                    <Typography
-                                        id="download-jupyterhub"
-                                        level="h6"
-                                        fontWeight="lg"
-                                        mb={1}
-                                    >
-                                        To download the data on I-GUIDE Platform JupyterHub:
-                                    </Typography>
-                                    <CopyBlock
-                                        language={'shell'}
-                                        text={downloadInstruction}
-                                        showLineNumbers={false}
-                                        theme={dracula}
-                                        wrapLines={true}
-                                        codeBlock
-                                    />
-                                </Box>
-                                <Typography
-                                    id="notebook-ds"
-                                    level="h5"
-                                    fontWeight="lg"
-                                    mb={1}
-                                >
-                                    Related notebooks:
-                                </Typography>
-                                <List aria-labelledby="decorated-list-demo">
-                                    {relatedNotebooks.map((notebook) => (
-                                        <Link
-                                            key={notebook}
-                                            underline="none"
-                                            href={`/notebooks/${notebook}`}
-                                            sx={{ color: 'text.tertiary' }}
-                                        >
-                                            <ListItem>
-                                                <ListItemButton>
-                                                    {extractValueFromJSON('id', notebook, 'title', notebooks)}
-                                                </ListItemButton>
-                                            </ListItem>
-                                        </Link>
-                                    ))}
-                                </List>
+                                <CodeSnippet directDownloadLink={directDownloadLink} />
+                                <RelatedResourcesList title="Related notebooks" relatedResourcesIds={relatedNotebooks} relatedResourceType="notebook" />
                                 <Box sx={{ display: 'flex', gap: 2, alignItems: 'center' }}>
                                     <Button size="sm">
                                         <Link
--- src/utils/DataAccessCodeGenerator.js
+++ src/utils/DataAccessCodeGenerator.js
@@ -1,3 +1,8 @@
+/**
+ * Identify the type of the download link
+ * @param {string} inputString the download link
+ * @return {string} the type of the inputString
+ */
 function identifyInputType(inputString) {
     const urlPattern = new RegExp(
         '^(https?:\\/\\/)?' + // http:// or https://
@@ -20,41 +25,41 @@ function identifyInputType(inputString) {
     } else if (s3BucketPattern.test(inputString)) {
         return "S3 Bucket Name";
     } else {
-        return "Invalid Input";
+        return "Invalid inputLink";
     }
 }
 
-export function generateDataAccessCode(input, platform) {
-    const inputType = identifyInputType(input);
+/**
+ * Get the download instruction given a link for a given platform
+ * @param {string} inputLink the link to the resource
+ * @param {string} platform the platform which the snippet is generated for. It's either iguide or python
+ * @return {string} the download instruction
+ */
+export function generateDataAccessCode(inputLink, platform) {
+    const inputType = identifyInputType(inputLink);
 
     let responseMessage;
 
     if (platform === 'python') {
         if (inputType === "URL") {
-            responseMessage = `from urllib.request import urlretrieve\nurlretrieve("${input}")`;
+            responseMessage = `from urllib.request import urlretrieve\nurlretrieve("${inputLink}")`;
         } else if (inputType === "S3 Bucket Name") {
-            responseMessage = `import boto3\nfrom botocore import UNSIGNED\nfrom botocore.config import Config\nimport os\n\nbucket_name = "${input}"\npath = "./"\ns3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))\ns3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))\nbucket = s3.Bucket(bucket_name)\nfor obj in bucket.objects.all():\n    key = obj.key\n    print(key)\n    file_path = path + key\n    if not os.path.isfile(file_path):\n        s3_client.download_file(bucket_name, key, file_path)`;
+            responseMessage = `import boto3\nfrom botocore import UNSIGNED\nfrom botocore.config import Config\nimport os\n\nbucket_name = "${inputLink}"\npath = "./"\ns3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))\ns3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))\nbucket = s3.Bucket(bucket_name)\nfor obj in bucket.objects.all():\n    key = obj.key\n    print(key)\n    file_path = path + key\n    if not os.path.isfile(file_path):\n        s3_client.download_file(bucket_name, key, file_path)`;
         } else if (inputType === "S3 Bucket Name and Key") {
-            const [bucketName, key] = input.split('/', 2);
+            const [bucketName, key] = inputLink.split('/', 2);
             responseMessage = `import boto3\nfrom botocore import UNSIGNED\nfrom botocore.config import Config\nimport os\n\nkey = "${key}"\nbucket_name = "${bucketName}"\npath = "./"\nif not os.path.isfile(path+key):\n    s3 = boto3.resource('s3', config=Config(signature_version=UNSIGNED))\n    s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))\n    s3_client.download_file(bucket_name, key, path + key)\nelse:\n    print(file_path + " already downloaded")`;
         } else if (inputType === "S3 Bucket Name and Path") {
-            const [bucketName, ...pathArray] = input.split('/');
+            const [bucketName, ...pathArray] = inputLink.split('/');
             const path = pathArray.join('/');
             responseMessage = `import boto3\nfrom botocore import UNSIGNED\nfrom botocore.config import Config\nimport os\n\nbucket_name = "${bucketName}"\npath = "${path}"\nsession = boto3.Session()\ns3 = session.client('s3', config=Config(signature_version=UNSIGNED))\nresponse = s3.list_objects_v2(Bucket=bucket_name, Prefix=path)\nfor obj in response['Contents']:\n    if (obj['Key'] === path) {\n        continue;\n    }\n    file_name = obj['Key'].split("/").pop()\n    if (!os.path.isfile(path + file_name)) {\n        s3_client.download_file(bucket_name, obj['Key'], path + file_name)\n    } else {\n        print(path + file_name + " already downloaded")\n    }\n}`;
         } else {
-            responseMessage = "Invalid Input";
+            responseMessage = "Invalid inputLink";
         }
     } else if (platform === 'iguide') {
-        responseMessage = `download_to_notebook("${input}")`;
+        responseMessage = `download_to_notebook("${inputLink}")`;
     } else {
         responseMessage = "Invalid Action";
     }
     
     return responseMessage;
 }
-
-// Example usage:
-const input = "data/test";
-const platform = "python";
-const message = generateDataAccessCode(input, platform);
-console.log(message);
