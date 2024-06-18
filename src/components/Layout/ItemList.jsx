7b2b7236ce64ea227e2705f66b8fc7a23f991b06
Erick Li <ericklizh@gmail.com>
2024-06-18 14:41:24 -0500

    Adjust return result list padding

--- src/components/Layout/ItemList.jsx
+++ src/components/Layout/ItemList.jsx
@@ -64,7 +64,8 @@ export default function ItemList(props) {
                         sx={{
                             backgroundColor: 'inherit',
                             px: { xs: 2, md: 4 },
-                            py: 2,
+                            pt: 4,
+                            pb: 8,
                             borderBottom: '1px solid',
                             borderColor: 'divider',
                         }}
