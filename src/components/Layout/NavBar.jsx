af4debb54ec4c98e6c1ef8a7ce49a1f79abaa8ca
Erick Li <ericklizh@gmail.com>
2024-06-26 18:14:40 -0500

    Add login button on the navbar

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -61,88 +61,115 @@ export default function NavBar() {
                         src="/images/Logo.png"
                     />
                 </Link>
-                <Button
-                    ref={buttonRef}
-                    id="composition-button"
-                    aria-controls={'composition-menu'}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    variant="outlined"
-                    color="neutral"
-                    onClick={() => {
-                        setOpen(!open);
-                    }}
+                <Stack
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="center"
+                    spacing={1}
+                    sx={{ display: { xs: 'flex', sm: 'none' } }}
                 >
-                    <MenuIcon />
-                </Button>
-                <Popup
-                    role={undefined}
-                    id="composition-menu"
-                    open={open}
-                    anchorEl={buttonRef.current}
-                    disablePortal
-                    modifiers={[
-                        {
-                            name: 'offset',
-                            options: {
-                                offset: [0, 4],
-                            },
-                        },
-                    ]}
-                >
-                    <ClickAwayListener
-                        onClickAway={(event) => {
-                            if (event.target !== buttonRef.current) {
-                                handleClose();
-                            }
+                    <Link key="login" to={'#'} style={{ textDecoration: 'none' }}>
+                        <Button size="lg" variant={'outlined'} color="primary">
+                            Login
+                        </Button>
+                    </Link>
+                    <Button
+                        size="lg"
+                        ref={buttonRef}
+                        id="composition-button"
+                        aria-controls={'composition-menu'}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        variant="outlined"
+                        color="neutral"
+                        onClick={() => {
+                            setOpen(!open);
                         }}
                     >
-                        <MenuList
-                            variant="outlined"
-                            onKeyDown={handleListKeyDown}
-                            sx={{ boxShadow: 'md' }}
+                        <MenuIcon />
+                    </Button>
+                    <Popup
+                        role={undefined}
+                        id="composition-menu"
+                        open={open}
+                        anchorEl={buttonRef.current}
+                        disablePortal
+                        modifiers={[
+                            {
+                                name: 'offset',
+                                options: {
+                                    offset: [0, 4],
+                                },
+                            },
+                        ]}
+                    >
+                        <ClickAwayListener
+                            onClickAway={(event) => {
+                                if (event.target !== buttonRef.current) {
+                                    handleClose();
+                                }
+                            }}
                         >
-                            {pages?.map((page) => (
-                                <Link key={page[1]} to={page[1]} style={{ textDecoration: 'none' }}>
-                                    <MenuItem key={page[0]}>
-                                        <Typography textAlign="center">{page[0]}</Typography>
-                                    </MenuItem>
-                                </Link>
-                            ))}
-                        </MenuList>
-                    </ClickAwayListener>
-                </Popup>
+                            <MenuList
+                                variant="outlined"
+                                onKeyDown={handleListKeyDown}
+                                sx={{ boxShadow: 'md' }}
+                            >
+                                {pages?.map((page) => (
+                                    <Link key={page[1]} to={page[1]} style={{ textDecoration: 'none' }}>
+                                        <MenuItem key={page[0]}>
+                                            <Typography textAlign="center">{page[0]}</Typography>
+                                        </MenuItem>
+                                    </Link>
+                                ))}
+                            </MenuList>
+                        </ClickAwayListener>
+                    </Popup>
+                </Stack>
             </Stack>
 
             {/* When page is wider than 600px */}
             <Stack
                 direction="row"
-                justifyContent="flex-start"
+                justifyContent="space-between"
                 alignItems="center"
                 spacing={1}
                 sx={{ display: { xs: 'none', sm: 'flex' } }}
             >
-                <Link to={'https://i-guide.io'} style={{ textDecoration: 'none' }}>
-                    <Box
-                        component="img"
-                        sx={{ height: 40, mt: 1, px: 2 }}
-                        alt="Logo"
-                        src="/images/Logo.png"
-                    />
-                </Link>
-                {pages?.map((page) => (
-                    <Link key={page[1]} to={page[1]} style={{ textDecoration: 'none' }}>
-                        <Button
-                            key={page[0]}
-                            variant="plain"
-                            color="neutral"
-                            size="sm"
-                            sx={{ alignSelf: 'center' }}
-                        >
-                            {page[0]}
-                        </Button>
+                <Stack
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="center"
+                    spacing={1}
+                    sx={{ display: { xs: 'none', sm: 'flex' } }}
+                >
+                    <Link to={'https://i-guide.io'} style={{ textDecoration: 'none' }}>
+                        <Box
+                            component="img"
+                            sx={{ height: 40, mt: 1, px: 2 }}
+                            alt="Logo"
+                            src="/images/Logo.png"
+                        />
                     </Link>
-                ))}
+                    {pages?.map((page) => (
+                        <Link key={page[1]} to={page[1]} style={{ textDecoration: 'none' }}>
+                            <Button
+                                key={page[0]}
+                                variant="plain"
+                                color="neutral"
+                                size="sm"
+                                sx={{ alignSelf: 'center' }}
+                            >
+                                {page[0]}
+                            </Button>
+                        </Link>
+                    ))}
+                </Stack>
+                <Link key="login" to={'#'} style={{ textDecoration: 'none' }}>
+                    <Button size="lg" variant={'outlined'} color="primary">
+                        Login
+                    </Button>
+                </Link>
             </Stack>
         </Box>
     );
