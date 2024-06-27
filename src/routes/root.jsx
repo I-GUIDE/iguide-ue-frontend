b904ccd4b15c09075c624724d323d48d6a2bf77e
Erick Li <ericklizh@gmail.com>
2024-06-27 13:12:29 -0500

    1st stage cilogon

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,7 +5,35 @@ import { StyledEngineProvider } from '@mui/material/styles';
 import NavBar from '../components/Layout/NavBar.jsx';
 import Footer from '../components/Layout/Footer.jsx';
 
+import { useAuth } from "react-oidc-context";
+
 export default function Root() {
+    const auth = useAuth();
+
+    switch (auth.activeNavigator) {
+        case "signinSilent":
+            return <div>Signing you in...</div>;
+        case "signoutRedirect":
+            return <div>Signing you out...</div>;
+    }
+
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (auth.error) {
+        return <div>Oops... {auth.error.message}</div>;
+    }
+
+    if (auth.isAuthenticated) {
+        return (
+        <div>
+            Hello {auth.user?.profile.sub}{" "}
+            <button onClick={() => void auth.removeUser()}>Log out</button>
+        </div>
+        );
+    }
+
     return (
         <StyledEngineProvider injectFirst>
             <NavBar />
@@ -13,6 +41,7 @@ export default function Root() {
                 <Outlet />
             </div>
             <Footer />
+            <button onClick={() => void auth.signinRedirect()}>Log in</button>
         </StyledEngineProvider>
     )
 }
