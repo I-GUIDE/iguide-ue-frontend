9fd6345d361943040462fddd1e3d1a712d016fc6
Erick Li <ericklizh@gmail.com>
2024-06-27 18:42:07 -0500

    Remove test button

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,40 +5,7 @@ import { StyledEngineProvider } from '@mui/material/styles';
 import NavBar from '../components/Layout/NavBar.jsx';
 import Footer from '../components/Layout/Footer.jsx';
 
-import { useAuth } from "react-oidc-context";
-
 export default function Root() {
-    const auth = useAuth();
-
-    switch (auth.activeNavigator) {
-        case "signinSilent":
-            return <div>Signing you in...</div>;
-        case "signoutRedirect":
-            return <div>Signing you out...</div>;
-    }
-
-    if (auth.isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (auth.error) {
-        return (
-            <div>
-                <p>An error occurred during authentication:</p>
-                <pre>{auth.error.message}</pre>
-            </div>
-        );
-    }
-
-    if (auth.isAuthenticated) {
-        return (
-            <div>
-                Hello {auth.user?.profile.sub}{" "}
-                <button onClick={() => void auth.removeUser()}>Log out</button>
-            </div>
-        );
-    }
-
     return (
         <StyledEngineProvider injectFirst>
             <NavBar />
@@ -46,7 +13,6 @@ export default function Root() {
                 <Outlet />
             </div>
             <Footer />
-            <button onClick={() => void auth.signinRedirect()}>Log in</button>
         </StyledEngineProvider>
     )
 }
