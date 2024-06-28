a68702571b9876e924b5cd75529282d28837414a
Erick Li <ericklizh@gmail.com>
2024-06-28 09:27:47 -0500

    Create logincallback func

--- src/routes/auth/loginCallback.jsx
+++ src/routes/auth/loginCallback.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
 import { useAuth } from "react-oidc-context";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export default function AuthCallback() {
     const auth = useAuth();
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
+    const [isAuth, setIsAuth] = useState(false);
 
     useEffect(() => {
         console.log(auth)
         if (auth.isAuthenticated) {
-            navigate('/user_profile');
+            setIsAuth(true)   
+            // navigate('/user_profile');
+        } else {
+            setIsAuth(false)
         }
         // auth.isAuthenticated.then(() => {
         //     navigate('/userprofile');
         // });
-    }, []);
+    }, [auth]);
 
-    return <div>Processing signin...</div>;
+    if (isAuth) {
+        return <Navigate to="/user_profile" />
+    } else {
+        return <div>Processing signin...</div>;
+    }
+    
 }
