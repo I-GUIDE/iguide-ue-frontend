a0a257ba0b224c73343341836266852263a7818e
Erick Li <ericklizh@gmail.com>
2024-07-01 08:51:08 -0500

    Control access to user profiles

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -8,90 +8,104 @@ import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
 import Sheet from '@mui/joy/Sheet';
 
+const AUTH_BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
+
 export default function UserCard(props) {
     const user = props.user;
+    
     if (user) {
-        console.log('profile', user.passport)
-    }
+        // If user.passport exists, that means the user is logged in.
+        if (user.passport) {
+            console.log('passport yes', user)
+            console.log('profile', user.passport)
 
-    return (
-        <Box
-            sx={{
-                width: '100%',
-                position: 'relative',
-                overflow: { xs: 'auto', sm: 'initial' },
-            }}
-        >
-            <Card
-                orientation="horizontal"
-                sx={{
-                    width: '100%',
-                    flexWrap: 'wrap',
-                    [`& > *`]: {
-                        '--stack-point': '500px',
-                        minWidth:
-                            'clamp(0px, (calc(var(--stack-point) - 2 * var(--Card-padding) - 2 * var(--variant-borderWidth, 0px)) + 1px - 100%) * 999, 100%)',
-                    },
-                    // make the card resizable for demo
-                    overflow: 'auto',
-                    resize: 'horizontal',
-                }}
-            >
-                <AspectRatio flex ratio="1" maxHeight={182} sx={{ minWidth: 182 }}>
-                    <img
-                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-                        srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
-                        loading="lazy"
-                        alt=""
-                    />
-                </AspectRatio>
-                <CardContent>
-                    <Typography fontSize="xl" fontWeight="lg">
-                        {user ? user.passport.user.name : "no name"}
-                    </Typography>
-                    <Typography level="body-sm" fontWeight="lg" textColor="text.tertiary">
-                        {user ? user.passport.user.sub : "no sub"}
-                    </Typography>
-                    <Sheet
+            return (
+                <Box
+                    sx={{
+                        width: '100%',
+                        position: 'relative',
+                        overflow: { xs: 'auto', sm: 'initial' },
+                    }}
+                >
+                    <Card
+                        orientation="horizontal"
                         sx={{
-                            bgcolor: 'background.level1',
-                            borderRadius: 'sm',
-                            p: 1.5,
-                            my: 1.5,
-                            display: 'flex',
-                            gap: 2,
-                            '& > div': { flex: 1 },
+                            width: '100%',
+                            flexWrap: 'wrap',
+                            [`& > *`]: {
+                                '--stack-point': '500px',
+                                minWidth:
+                                    'clamp(0px, (calc(var(--stack-point) - 2 * var(--Card-padding) - 2 * var(--variant-borderWidth, 0px)) + 1px - 100%) * 999, 100%)',
+                            },
+                            // make the card resizable for demo
+                            overflow: 'auto',
+                            resize: 'horizontal',
                         }}
                     >
-                        <div>
-                            <Typography level="body-xs" fontWeight="lg">
-                                Contributions
+                        <AspectRatio flex ratio="1" maxHeight={182} sx={{ minWidth: 182 }}>
+                            <img
+                                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
+                                srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
+                                loading="lazy"
+                                alt=""
+                            />
+                        </AspectRatio>
+                        <CardContent>
+                            <Typography fontSize="xl" fontWeight="lg">
+                                {user ? user.passport.user.name : "no name"}
                             </Typography>
-                            <Typography fontWeight="lg">10</Typography>
-                        </div>
-                        <div>
-                            <Typography level="body-xs" fontWeight="lg">
-                                Followers
+                            <Typography level="body-sm" fontWeight="lg" textColor="text.tertiary">
+                                {user ? user.passport.user.sub : "no sub"}
                             </Typography>
-                            <Typography fontWeight="lg">230k</Typography>
-                        </div>
-                        <div>
-                            <Typography level="body-xs" fontWeight="lg">
-                                Rating
-                            </Typography>
-                            <Typography fontWeight="lg">10.0</Typography>
-                        </div>
-                    </Sheet>
-                    <Box sx={{ display: 'flex', gap: 1.5, '& > button': { flex: 1 } }}>
-                        <Button variant="outlined" color="neutral">
-                            Past Contributions
-                        </Button>
-                        <Button variant="solid" color="primary">
-                            Contribute
-                        </Button>
-                    </Box>
-                </CardContent>
-            </Card>
-        </Box>
-    )
+                            <Sheet
+                                sx={{
+                                    bgcolor: 'background.level1',
+                                    borderRadius: 'sm',
+                                    p: 1.5,
+                                    my: 1.5,
+                                    display: 'flex',
+                                    gap: 2,
+                                    '& > div': { flex: 1 },
+                                }}
+                            >
+                                <div>
+                                    <Typography level="body-xs" fontWeight="lg">
+                                        Contributions
+                                    </Typography>
+                                    <Typography fontWeight="lg">10</Typography>
+                                </div>
+                                <div>
+                                    <Typography level="body-xs" fontWeight="lg">
+                                        Followers
+                                    </Typography>
+                                    <Typography fontWeight="lg">230k</Typography>
+                                </div>
+                                <div>
+                                    <Typography level="body-xs" fontWeight="lg">
+                                        Rating
+                                    </Typography>
+                                    <Typography fontWeight="lg">10.0</Typography>
+                                </div>
+                            </Sheet>
+                            <Box sx={{ display: 'flex', gap: 1.5, '& > button': { flex: 1 } }}>
+                                <Button variant="outlined" color="neutral">
+                                    Past Contributions
+                                </Button>
+                                <Button variant="solid" color="primary">
+                                    Contribute
+                                </Button>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </Box>
+            )
+        // Otherwise... we need to redirect the user to the login page
+        } else {
+            console.log('passport no', user)
+            window.open(AUTH_BACKEND_URL + "/login", "_self");
+        }
+    // If the user doesn't exist, return null and wait for it.
+    } else {
+        return null;
+    }
 }
