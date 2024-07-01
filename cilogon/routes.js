a3dbcdeace00007e0e8b9198a4194abad722ff16
Erick Li <ericklizh@gmail.com>
2024-07-01 17:59:44 -0500

    When the user is not logged in, accessing the user profile page will lead them to CILogon

--- cilogon/routes.js
+++ cilogon/routes.js
@@ -31,9 +31,46 @@ router.get("/", (req, res) => {
     res.send(" <a href='/login'>Log In with OAuth 2.0 Provider </a>")
 });
 
-router.get("/user", (req, res) => {
+router.get("/auth-validation", function (req, res) {
     res.header("Content-Type", 'application/json');
-    res.end(JSON.stringify({ user: req.session }, null, 1));
+    if (req.isAuthenticated()) {
+        res.end(JSON.stringify({ isAuthenticated: true }))
+    } else {
+        res.end(JSON.stringify({ isAuthenticated: false }))
+    }
+})
+
+router.get("/userinfo", (req, res) => {
+    res.header("Content-Type", 'application/json');
+    if (req.session.passport) {
+        const sub = req.session.passport.user.sub ? req.session.passport.user.sub : null;
+        const given_name = req.session.passport.user.given_name ? req.session.passport.user.given_name : null;
+        const family_name = req.session.passport.user.family_name ? req.session.passport.user.family_name : null;
+        const idp_name = req.session.passport.user.idp_name ? req.session.passport.user.idp_name : null;
+        const email = req.session.passport.user.email ? req.session.passport.user.email : null;
+        const iss = req.session.passport.user.iss ? req.session.passport.user.iss : null;
+
+        const user_info = JSON.stringify({
+            userInfo: {
+                "sub": sub,
+                "given_name": given_name,
+                "family_name": family_name,
+                "idp_name": idp_name,
+                "email": email,
+                "iss": iss,
+            }
+        })
+
+        console.log('req passport', req.session.passport)
+        res.end(user_info);
+    } else {
+        const no_user_info = JSON.stringify({
+            userInfo: null
+        })
+
+        console.log('No users')
+        res.end(JSON.stringify(no_user_info));
+    }
 });
 
 
