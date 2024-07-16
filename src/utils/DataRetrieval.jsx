6a13ee72d731be76e588f7652bf846d37255ce7e
Erick Li <ericklizh@gmail.com>
2024-07-16 13:59:38 -0500

    Add docstrings

--- src/utils/DataRetrieval.jsx
+++ src/utils/DataRetrieval.jsx
@@ -173,6 +173,16 @@ export async function fetchResourcesByContributor(openid, sortBy = '_score', ord
     return response.json();
 }
 
+/**
+ * Fetches the title of elements of a given ID.
+ *
+ * @async
+ * @function fetchRelatedResourceTitles
+ * @param {string} type - The type of resources to fetch titles for.
+ * @param {Array<string>} ids - A list of IDs
+ * @returns {Promise<Array<string>>} A promise that resolves to an array of all titles.
+ * @throws {Error} Throws an error if the fetch operation fails.
+ */
 export async function fetchRelatedResourceTitles(type, ids) {
     const idString = ids.join(',');
     const response = await fetch(`${BACKEND_URL_PORT}/api/resources/${type}/${idString}`);
--- src/utils/UserManager.jsx
+++ src/utils/UserManager.jsx
@@ -1,5 +1,11 @@
 const USER_BACKEND_URL = import.meta.env.VITE_DATABASE_BACKEND_URL
 
+/**
+ * Fetch the information of a user given a uid
+ * @param {string} uid the user's OpenID
+ * @return {Promise<Array<Dict>>} the information of the user
+ * @throws {Error} Throws an error if fetching the user failed.
+ */
 export async function fetchUser(uid) {
     const openid = encodeURIComponent(uid);
     const response = await fetch(`${USER_BACKEND_URL}/api/users/${openid}`);
@@ -15,6 +21,17 @@ export async function fetchUser(uid) {
     return result;
 }
 
+/**
+ * Add a user
+ * @param {string} uid the OpenID of the user
+ * @param {string} first_name the first name of the user
+ * @param {string} last_name the last name of the user
+ * @param {string} email the work email of the user
+ * @param {string} affiliation the university of organization of the user
+ * @param {string} bio a short bio of the user
+ * @return {Promise<Array<Dict>>} information of whether the operation was successful
+ * @throws {Error} Throws an error if adding a user failed
+ */
 export async function addUser(uid, first_name, last_name, email, affiliation, bio) {
     const user = {
         openid: uid,
@@ -42,6 +59,12 @@ export async function addUser(uid, first_name, last_name, email, affiliation, bi
     return result;
 }
 
+/**
+ * Delete a user
+ * @param {string} uid the OpenID of the user
+ * @return {Promise<Array<Dict>>} information of whether the operation was successful
+ * @throws {Error} Throws an error if deleting a user failed
+ */
 export async function deleteUser(uid) {
     const openid = encodeURIComponent(uid);
     const response = await fetch(`${USER_BACKEND_URL}/api/users/${openid}`, {
@@ -57,6 +80,11 @@ export async function deleteUser(uid) {
     return result;
 }
 
+/**
+ * Delete a user
+ * @param {string} uid the OpenID of the user
+ * @return {Promise<boolean>} boolean value of whether the user exists
+ */
 export async function checkUser(uid) {
     const openid = encodeURIComponent(uid);
     const response = await fetch(`${USER_BACKEND_URL}/api/check_users/${openid}`);
