8107111d324ee29bee51568fc1da641071504bc2
Erick Li <ericklizh@gmail.com>
2024-08-02 16:13:35 -0500

    Responsive design for UserProfileContributionInfoCard

--- src/components/UserProfileContributionInfoCard.jsx
+++ src/components/UserProfileContributionInfoCard.jsx
@@ -9,13 +9,13 @@ import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
 import Sheet from '@mui/joy/Sheet';
 import Stack from '@mui/joy/Stack';
+import Grid from '@mui/joy/Grid';
 
 import { RESOURCE_TYPE_COLORS } from '../configs/ResourceTypes';
 
 import '../utils/UserManager';
 
 export default function UserProfileContributionInfoCard(props) {
-    const numberOfContributions = props.numberOfContributions;
     const [isAuthenticated, setIsAuthenticated, userInfo, setUserInfo, localUserInfo, setLocalUserInfo] = useOutletContext();
 
     // If the user info from the local DB is still not available, wait...
@@ -26,9 +26,11 @@ export default function UserProfileContributionInfoCard(props) {
     return (
         <Box
             sx={{
-                width: '60%',
+                maxWidth: 1000,
+                width: '50%',
                 position: 'relative',
-                overflow: { xs: 'auto', sm: 'initial' },
+                overflow: 'auto',
+                display: 'flex'
             }}
         >
             <Card
@@ -40,25 +42,65 @@ export default function UserProfileContributionInfoCard(props) {
                 }}
             >
                 <CardContent>
-                    <Stack direction="column" spacing={3} alignItems="center">
-                        <Typography level="h3" fontWeight="lg">
-                            Would you like to make a new knowledge contribution?
-                        </Typography>
-                        <Stack direction="row" spacing={2} alignItems="center">
+                    <Grid
+                        container
+                        spacing={2}
+                    >
+                        <Grid
+                            xs={12}
+                            justifyContent="center"
+                            alignItems="center"
+                            display="flex"
+                        >
+                            <Typography level="title-lg">
+                                Would you like to make a new knowledge contribution?
+                            </Typography>
+                        </Grid>
+                        <Grid
+                            xs={12}
+                            md={6}
+                            justifyContent="center"
+                            alignItems="center"
+                            display="flex"
+                        >
                             <Button component="a" href="/contribution/dataset" variant="outlined" color={RESOURCE_TYPE_COLORS["dataset"]}>
                                 New Dataset
                             </Button>
+                        </Grid>
+                        <Grid
+                            xs={12}
+                            md={6}
+                            justifyContent="center"
+                            alignItems="center"
+                            display="flex"
+                        >
                             <Button component="a" href="/contribution/notebook" variant="outlined" color={RESOURCE_TYPE_COLORS["notebook"]}>
                                 New Notebook
                             </Button>
+                        </Grid>
+                        <Grid
+                            xs={12}
+                            md={6}
+                            justifyContent="center"
+                            alignItems="center"
+                            display="flex"
+                        >
                             <Button component="a" href="/contribution/publication" variant="outlined" color={RESOURCE_TYPE_COLORS["publication"]}>
                                 New Publication
                             </Button>
+                        </Grid>
+                        <Grid
+                            xs={12}
+                            md={6}
+                            justifyContent="center"
+                            alignItems="center"
+                            display="flex"
+                        >
                             <Button component="a" href="/contribution/oer" variant="outlined" color={RESOURCE_TYPE_COLORS["oer"]}>
                                 New Educational Resource
                             </Button>
-                        </Stack>
-                    </Stack>
+                        </Grid>
+                    </Grid>
                 </CardContent>
             </Card>
         </Box>
