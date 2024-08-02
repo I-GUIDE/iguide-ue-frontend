5f0bdcab4f86be8405b0969dd82b69a6b84bff06
Erick Li <ericklizh@gmail.com>
2024-08-02 14:07:23 -0500

    Responsive design for navbar

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -14,6 +14,11 @@ import ListDivider from '@mui/joy/ListDivider';
 import MenuButton from '@mui/joy/MenuButton';
 import Dropdown from '@mui/joy/Dropdown';
 import Avatar from '@mui/joy/Avatar';
+import Drawer from '@mui/joy/Drawer';
+import List from '@mui/joy/List';
+import Divider from '@mui/joy/Divider';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton from '@mui/joy/ListItemButton';
 
 import { Popper } from '@mui/base/Popper';
 import { ClickAwayListener } from '@mui/base/ClickAwayListener';
@@ -32,6 +37,13 @@ export default function NavBar(props) {
 
     const buttonRef = React.useRef(null);
     const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (inOpen) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        setOpen(inOpen);
+    };
 
     const handleClose = () => {
         setOpen(false);
@@ -68,7 +80,7 @@ export default function NavBar(props) {
                     spacing="0.5rem"
                 >
                     <Button size="sm" component="a" href="https://jupyter.iguide.illinois.edu/" target="_blank" rel="noopener noreferrer">
-                        Launch JupyterHub
+                        JupyterHub
                     </Button>
                     <Dropdown>
                         <MenuButton color="primary">
@@ -111,7 +123,7 @@ export default function NavBar(props) {
                                 New Educational Resource
                             </MenuItem>
                             <ListDivider />
-                            <MenuItem variant="soft" onClick={logout}>
+                            <MenuItem onClick={logout}>
                                 Logout
                             </MenuItem>
                         </Menu>
@@ -128,12 +140,72 @@ export default function NavBar(props) {
                     spacing="0.5rem"
                 >
                     <Button size="sm" component="a" href="https://jupyter.iguide.illinois.edu/" target="_blank" rel="noopener noreferrer">
-                        Launch JupyterHub
+                        JupyterHub
                     </Button>
                     <Button size="sm" color="primary" onClick={login}>
                         Login
                     </Button>
                 </ButtonGroup>
+            )
+        }
+    }
+
+    function AuthInDrawer() {
+        if (isAuthenticated) {
+            return (
+                <List>
+                    <Link to={'/user-profile'} style={{ textDecoration: 'none' }}>
+                        <ListItem>
+                            My Profile
+                        </ListItem>
+                    </Link>
+                    <Link to={'/user-profile-update'} style={{ textDecoration: 'none' }}>
+                        <ListItem>
+                            Update Profile
+                        </ListItem>
+                    </Link>
+                    <Divider sx={{ my: 1 }}/>
+                    <Typography
+                        level="body-xs"
+                        textTransform="uppercase"
+                        fontWeight="lg"
+                        sx={{ px: 1.5, py: 1 }}
+                    >
+                        New Contribution
+                    </Typography>
+                    <Link to="/contribution/dataset" style={{ textDecoration: 'none' }}>
+                        <ListItem>
+                            New Dataset
+                        </ListItem>
+                    </Link>
+                    <Link to="/contribution/notebook" style={{ textDecoration: 'none' }}>
+                        <ListItem>
+                            New Notebook
+                        </ListItem>
+                    </Link>
+                    <Link to="/contribution/publication" style={{ textDecoration: 'none' }}>
+                        <ListItem>
+                            New Publication
+                        </ListItem>
+                    </Link>
+                    <Link to="/contribution/oer" style={{ textDecoration: 'none' }}>
+                        <ListItem>
+                            New Educational Resource
+                        </ListItem>
+                    </Link>
+                    <Divider sx={{ my: 1 }} />
+                    <ListItem onClick={logout}>
+                        Logout
+                    </ListItem>
+                </List>
+            )
+        } else {
+            return (
+                <List>
+                    <ListItem size="sm" color="primary" onClick={login}>
+                        <ListItemButton>Login</ListItemButton>
+                    </ListItem>
+                </List>
             )
         }
     }
@@ -148,30 +220,19 @@ export default function NavBar(props) {
                 display: 'auto'
             }}
         >
-            {/* When page is narrower than 600px */}
+            {/* When page is narrower than 960px */}
             <Stack
                 direction="row"
-                justifyContent="space-between"
+                justifyContent="flex-start"
                 alignItems="center"
-                spacing={1}
-                sx={{ display: { xs: 'flex', sm: 'none' } }}
+                spacing={2}
+                sx={{ display: { xs: 'flex', md: 'none' } }}
             >
-                <Link to={'https://i-guide.io'} style={{ textDecoration: 'none' }}>
-                    <Box
-                        component="img"
-                        sx={{ height: 40, mt: 1, px: 2 }}
-                        alt="Logo"
-                        src="/images/Logo.png"
-                    />
-                </Link>
                 <Stack
                     direction="row"
                     justifyContent="flex-start"
                     alignItems="center"
-                    spacing={1}
-                    sx={{ display: { xs: 'flex', sm: 'none' } }}
                 >
-                    <AuthButton />
                     <Button
                         size="lg"
                         ref={buttonRef}
@@ -179,68 +240,53 @@ export default function NavBar(props) {
                         aria-controls={'composition-menu'}
                         aria-haspopup="true"
                         aria-expanded={open ? 'true' : undefined}
-                        variant="outlined"
+                        variant="plain"
                         color="neutral"
-                        onClick={() => {
-                            setOpen(!open);
-                        }}
+                        onClick={toggleDrawer(true)}
                     >
                         <MenuIcon />
                     </Button>
-                    <Popup
-                        role={undefined}
-                        id="composition-menu"
-                        open={open}
-                        anchorEl={buttonRef.current}
-                        disablePortal
-                        modifiers={[
-                            {
-                                name: 'offset',
-                                options: {
-                                    offset: [0, 4],
-                                },
-                            },
-                        ]}
-                    >
-                        <ClickAwayListener
-                            onClickAway={(event) => {
-                                if (event.target !== buttonRef.current) {
-                                    handleClose();
-                                }
-                            }}
+                    <Drawer open={open} onClose={toggleDrawer(false)}>
+                        <Box
+                            role="presentation"
+                            onClick={toggleDrawer(false)}
+                            onKeyDown={toggleDrawer(false)}
+                            sx={{ p: 2 }}
                         >
-                            <MenuList
-                                variant="outlined"
-                                onKeyDown={handleListKeyDown}
-                                sx={{ boxShadow: 'md' }}
-                            >
+                            <List>
                                 {pages?.map((page) => (
                                     <Link key={page[1]} to={page[1]} style={{ textDecoration: 'none' }}>
-                                        <MenuItem key={page[0]}>
+                                        <ListItem key={page[0]}>
                                             <Typography textAlign="center">{page[0]}</Typography>
-                                        </MenuItem>
+                                        </ListItem>
                                     </Link>
                                 ))}
-                            </MenuList>
-                        </ClickAwayListener>
-                    </Popup>
+                            </List>
+                            <Divider sx={{ my: 1 }} />
+                            <AuthInDrawer />
+                        </Box>
+                    </Drawer>
+                    <Box
+                        component="img"
+                        sx={{ height: 40, mt: 1, px: 2 }}
+                        alt="Logo"
+                        src="/images/Logo.png"
+                    />
                 </Stack>
+
             </Stack>
 
-            {/* When page is wider than 600px */}
+            {/* When page is wider than 960px */}
             <Stack
                 direction="row"
                 justifyContent="space-between"
                 alignItems="center"
-                spacing={1}
-                sx={{ display: { xs: 'none', sm: 'flex' } }}
+                sx={{ display: { xs: 'none', md: 'flex' } }}
             >
                 <Stack
                     direction="row"
                     justifyContent="flex-start"
                     alignItems="center"
-                    spacing={1}
-                    sx={{ display: { xs: 'none', sm: 'flex' } }}
                 >
                     <Link to={'https://i-guide.io'} style={{ textDecoration: 'none' }}>
                         <Box
