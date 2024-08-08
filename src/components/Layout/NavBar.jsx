d6fbf59409644cd14e3f073fa89c24aa1132fe32
Erick Li <ericklizh@gmail.com>
2024-08-08 11:34:23 -0500

    Make the navbar sticky on top of the page

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,7 +1,17 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 
+import { Link } from "react-router-dom";
 import Jdenticon from "react-jdenticon";
+
+import {
+  experimental_extendTheme as materialExtendTheme,
+  Experimental_CssVarsProvider as MaterialCssVarsProvider,
+  THEME_ID as MATERIAL_THEME_ID,
+} from "@mui/material/styles";
+import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+const materialTheme = materialExtendTheme();
+import AppBar from "@mui/material/AppBar";
 
 import Box from "@mui/joy/Box";
 import Typography from "@mui/joy/Typography";
@@ -188,155 +198,180 @@ export default function NavBar(props) {
   }
 
   return (
-    <Box
-      sx={{
-        height: 70,
-        pt: 1,
-        mx: 2,
-        display: "auto",
-      }}
-    >
-      {/* When page is narrower than 960px */}
-      <Stack
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="center"
-        spacing={2}
-        sx={{ display: { xs: "flex", lg: "none" } }}
-      >
-        <Stack direction="row" justifyContent="flex-start" alignItems="center">
-          <Button
-            size="lg"
-            ref={buttonRef}
-            id="composition-button"
-            aria-controls={"composition-menu"}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            variant="plain"
-            color="neutral"
-            onClick={toggleDrawer(true)}
+    <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
+      <JoyCssVarsProvider>
+        <CssBaseline enableColorScheme />
+        <AppBar position="sticky" color="inherit">
+          <Box
+            sx={{
+              height: 70,
+              pt: 1,
+              mx: 2,
+              display: "auto",
+              bgcolor: "neutral",
+            }}
           >
-            <MenuIcon />
-          </Button>
-          <Drawer open={open} onClose={toggleDrawer(false)}>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                gap: 0.5,
-                ml: "auto",
-                mt: 1,
-                mr: 2,
-              }}
+            {/* When page is narrower than 960px */}
+            <Stack
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="center"
+              spacing={2}
+              sx={{ display: { xs: "flex", lg: "none" } }}
             >
-              <Typography
-                component="label"
-                htmlFor="close-icon"
-                fontSize="sm"
-                fontWeight="lg"
-                sx={{ cursor: "pointer" }}
+              <Stack
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="center"
               >
-                Close
-              </Typography>
-              <ModalClose id="close-icon" sx={{ position: "initial" }} />
-            </Box>
-            <Box sx={{ px: 2, py: 1 }}>
-              <SearchBar onSearch={() => setOpen(false)} />
-            </Box>
-            <Box
-              onClick={toggleDrawer(false)}
-              onKeyDown={toggleDrawer(false)}
-              sx={{ px: 2, py: 1 }}
+                <Button
+                  size="lg"
+                  ref={buttonRef}
+                  id="composition-button"
+                  aria-controls={"composition-menu"}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  variant="plain"
+                  color="neutral"
+                  onClick={toggleDrawer(true)}
+                >
+                  <MenuIcon />
+                </Button>
+                <Drawer open={open} onClose={toggleDrawer(false)}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      gap: 0.5,
+                      ml: "auto",
+                      mt: 1,
+                      mr: 2,
+                    }}
+                  >
+                    <Typography
+                      component="label"
+                      htmlFor="close-icon"
+                      fontSize="sm"
+                      fontWeight="lg"
+                      sx={{ cursor: "pointer" }}
+                    >
+                      Close
+                    </Typography>
+                    <ModalClose id="close-icon" sx={{ position: "initial" }} />
+                  </Box>
+                  <Box sx={{ px: 2, py: 1 }}>
+                    <SearchBar onSearch={() => setOpen(false)} />
+                  </Box>
+                  <Box
+                    onClick={toggleDrawer(false)}
+                    onKeyDown={toggleDrawer(false)}
+                    sx={{ px: 2, py: 1 }}
+                  >
+                    <List>
+                      {pages?.map((page) => (
+                        <Link
+                          key={page[1]}
+                          to={page[1]}
+                          style={{ textDecoration: "none" }}
+                        >
+                          <ListItem key={page[0]}>
+                            <Typography textAlign="center">
+                              {page[0]}
+                            </Typography>
+                          </ListItem>
+                        </Link>
+                      ))}
+                    </List>
+                    <Divider sx={{ my: 1 }} />
+                    <AuthInDrawer />
+                  </Box>
+                </Drawer>
+                <Box
+                  component="img"
+                  sx={{ height: 40, mt: 1, px: 2 }}
+                  alt="Logo"
+                  src="/images/Logo.png"
+                />
+              </Stack>
+            </Stack>
+
+            {/* When page is wider than 960px */}
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              sx={{ display: { xs: "none", lg: "flex" } }}
             >
-              <List>
+              <Stack
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="center"
+              >
+                <Link
+                  to={"https://i-guide.io"}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Box
+                    component="img"
+                    sx={{ height: 40, mt: 1, px: 2 }}
+                    alt="Logo"
+                    src="/images/Logo.png"
+                  />
+                </Link>
                 {pages?.map((page) => (
                   <Link
                     key={page[1]}
                     to={page[1]}
                     style={{ textDecoration: "none" }}
                   >
-                    <ListItem key={page[0]}>
-                      <Typography textAlign="center">{page[0]}</Typography>
-                    </ListItem>
+                    <Button
+                      key={page[0]}
+                      variant="plain"
+                      color="neutral"
+                      size="sm"
+                      sx={{ alignSelf: "center" }}
+                    >
+                      {page[0]}
+                    </Button>
                   </Link>
                 ))}
-              </List>
-              <Divider sx={{ my: 1 }} />
-              <AuthInDrawer />
-            </Box>
-          </Drawer>
-          <Box
-            component="img"
-            sx={{ height: 40, mt: 1, px: 2 }}
-            alt="Logo"
-            src="/images/Logo.png"
-          />
-        </Stack>
-      </Stack>
-
-      {/* When page is wider than 960px */}
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        sx={{ display: { xs: "none", lg: "flex" } }}
-      >
-        <Stack direction="row" justifyContent="flex-start" alignItems="center">
-          <Link to={"https://i-guide.io"} style={{ textDecoration: "none" }}>
-            <Box
-              component="img"
-              sx={{ height: 40, mt: 1, px: 2 }}
-              alt="Logo"
-              src="/images/Logo.png"
-            />
-          </Link>
-          {pages?.map((page) => (
-            <Link key={page[1]} to={page[1]} style={{ textDecoration: "none" }}>
-              <Button
-                key={page[0]}
-                variant="plain"
-                color="neutral"
-                size="sm"
-                sx={{ alignSelf: "center" }}
+              </Stack>
+              <Stack
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="center"
+                spacing={0.1}
               >
-                {page[0]}
-              </Button>
-            </Link>
-          ))}
-        </Stack>
-        <Stack
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-          spacing={0.1}
-        >
-          <SearchBar />
-          <ButtonGroup
-            color="primary"
-            orientation="horizontal"
-            size="sm"
-            variant="plain"
-            spacing="0.1rem"
-          >
-            <Tooltip title="Go to I-GUIDE JupyterHub" variant="solid">
-              <Button
-                size="sm"
-                component="a"
-                href="https://jupyter.iguide.illinois.edu/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Avatar
+                <SearchBar />
+                <ButtonGroup
+                  color="primary"
+                  orientation="horizontal"
+                  size="sm"
                   variant="plain"
-                  alt="jupyterhub"
-                  src="/images/Jupyter-logo.png"
-                />
-              </Button>
-            </Tooltip>
-            <AuthButton />
-          </ButtonGroup>
-        </Stack>
-      </Stack>
-    </Box>
+                  spacing="0.1rem"
+                >
+                  <Tooltip title="Go to I-GUIDE JupyterHub" variant="solid">
+                    <Button
+                      size="sm"
+                      component="a"
+                      href="https://jupyter.iguide.illinois.edu/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Avatar
+                        variant="plain"
+                        alt="jupyterhub"
+                        src="/images/Jupyter-logo.png"
+                      />
+                    </Button>
+                  </Tooltip>
+                  <AuthButton />
+                </ButtonGroup>
+              </Stack>
+            </Stack>
+          </Box>
+        </AppBar>
+      </JoyCssVarsProvider>
+    </MaterialCssVarsProvider>
   );
 }
