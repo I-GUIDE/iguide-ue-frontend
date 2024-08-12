2fd62acf0a2f183720e20bf35da69de056726523
Yunfan Kang <yfkang@illinois.edu>
2024-08-12 15:05:15 +0000

    Seperate JWT function to FetcherWithJWT.jsx

--- cilogon/routes.js
+++ cilogon/routes.js
@@ -1,16 +1,33 @@
-const router = require("express").Router();
-const passport = require("passport");
-const jwt = require("jsonwebtoken");
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const { Client } = require('@opensearch-project/opensearch');
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const router = express.Router();
 
 const os_node = process.env.OPENSEARCH_NODE;
 const os_usr = process.env.OPENSEARCH_USERNAME;
 const os_pswd = process.env.OPENSEARCH_PASSWORD;
 const os_index = process.env.OPENSEARCH_INDEX;
+const user_index = process.env.USER_INDEX
+const target_domain = process.env.JWT_TARGET_DOMAIN;
+
+console.log('OpenSearch Node:', os_node);
+console.log('OpenSearch Username:', os_usr);
+console.log('OpenSearch Password:', os_pswd);
+console.log('OpenSearch Index:', os_index);
+console.log('User Index: ', user_index);
+console.log('Target JWT backend domain: ', target_domain);
+
+if (!os_node) {
+  throw new Error('Missing OpenSearch node configuration');
+}
 
 const client = new Client({
-  node: os_node, // OpenSearch endpoint
+  node: os_node,
   auth: {
     username: os_usr,
     password: os_pswd,
@@ -21,6 +38,35 @@ const client = new Client({
 });
 
 const FRONTEND_URL = process.env.REACT_FRONTEND_URL;
+
+// Function to retrieve the role from the "user_dev" index
+const getUserRole = async (user_id) => {
+  const openid = decodeURIComponent(user_id);
+
+  try {
+    const response = await client.search({
+      index: user_index,
+      body: {
+        query: {
+          term: {
+            openid: openid
+          }
+        }
+      }
+    });
+
+    if (response.body.hits.total.value === 0) {
+      console.error('User not found');
+      return null;
+    }else{
+      return response.body.hits.hits[0]._source.role;
+    }
+  } catch (error) {
+    console.error('Error retrieving user role:', error);
+    return null;
+  }
+};
+
 
 // Store refresh token in OpenSearch
 const storeRefreshToken = async (client, token, user_id) => {
@@ -35,7 +81,7 @@ const storeRefreshToken = async (client, token, user_id) => {
 };
 
 const generateAccessToken = (user) => {
-    return jwt.sign(user, process.env.JWT_ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
+    return jwt.sign(user, process.env.JWT_ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
 };
 
 const generateRefreshToken = (user) => {
@@ -46,19 +92,29 @@ router.get('/login', function (req, res, next) {
   console.log('-----------------------------');
   console.log('/Start login handler');
   next();
-}, passport.authenticate('oidc', { scope: "openid profile email org.cilogon.userinfo" }));
+}, passport.authenticate('oidc', { scope: "openid profile email org.cilogon.userinfo",
+initialidp: "urn:mace:incommon:uiuc.edu" }));
 
 router.get('/cilogon-callback', async (req, res, next) => {
   passport.authenticate('oidc', async (err, user, info) => {
-    if (err || !user) {
-      return res.redirect(`/`);
+    if (err) {
+      console.log(err);
+      return res.redirect(`/error`);
+    }
+    if (!user){
+      return res.redirect(`/nouser`);
     }
     req.logIn(user, async function (err) {
       if (err) {
-        return res.redirect(`/`);
+        return res.redirect(`/errorlogin`);
       }
+
+      // Retrieve user role from OpenSearch
+      const role = await getUserRole(user.sub) || 'untrusted_user'; // Default to 'user' if role not found
+      console.log('user role: ', role);
+
       // Generate JWT token with role
-      const userPayload = { id: user.sub, role: 'user' }; // Determine role dynamically as needed
+      const userPayload = { id: user.sub, role };
 
       // Generate tokens
       const accessToken = generateAccessToken(userPayload);
@@ -68,8 +124,10 @@ router.get('/cilogon-callback', async (req, res, next) => {
       await storeRefreshToken(client, refreshToken, user.sub);
 
       // Set the tokens in cookies
-      res.cookie('jwt', accessToken, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
-      res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
+      res.cookie('jwt', accessToken, { httpOnly: true, secure: process.env.NODE_ENV === 'production' , sameSite: 'None', domain: target_domain, path: '/'});
+      res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: 'None', domain: target_domain, path: '/' });
+      
+      console.log("Setting cookies to: ", target_domain);
 
       res.redirect(`${FRONTEND_URL}/user-profile`);
     });
@@ -77,8 +135,8 @@ router.get('/cilogon-callback', async (req, res, next) => {
 });
 
 router.get('/logout', function (req, res) {
-  res.clearCookie('jwt');
-  res.clearCookie('refreshToken');
+  res.clearCookie('jwt', { httpOnly: true, secure: process.env.NODE_ENV === 'production' , sameSite: 'None', domain: target_domain, path: '/'});
+  res.clearCookie('refreshToken', { httpOnly: true, secure: process.env.NODE_ENV === 'production' , sameSite: 'None', domain: target_domain, path: '/'});
   req.session.destroy(function (err) {
     res.redirect(FRONTEND_URL);
   });
