9b54f174ab5c20053b220ba90c2696efc5373adf
Erick Li <ericklizh@gmail.com>
2024-08-12 10:17:50 -0500

    Update syntax

--- src/utils/FetcherWithJWT.jsx
+++ src/utils/FetcherWithJWT.jsx
@@ -1,35 +1,35 @@
 const BACKEND_URL_PORT = import.meta.env.VITE_DATABASE_BACKEND_URL;
 export async function fetchWithAuth(url, options = {}) {
-    let res = await fetch(url, {
-        ...options,
-        credentials: 'include' // Ensure cookies are sent with the request
+  let res = await fetch(url, {
+    ...options,
+    credentials: "include", // Ensure cookies are sent with the request
+  });
+
+  if (res.status === 401) {
+    console.log("Token expired, try to refresh");
+    // Access token has expired, refresh it
+    await refreshAccessToken();
+
+    // Retry the original request
+    res = await fetch(url, {
+      ...options,
+      credentials: "include",
     });
+  }
 
-    if (res.status === 401) {
-        console.log("Token expired, try to refresh");
-        // Access token has expired, refresh it
-        await refreshAccessToken();
-
-        // Retry the original request
-        res = await fetch(url, {
-            ...options,
-            credentials: 'include'
-        });
-    }
-
-    return res;
+  return res;
 }
 
 export async function refreshAccessToken() {
-    const res = await fetch(`${BACKEND_URL_PORT}/api/refresh-token`, {
-        method: 'POST',
-        credentials: 'include'
-    });
+  const res = await fetch(`${BACKEND_URL_PORT}/api/refresh-token`, {
+    method: "POST",
+    credentials: "include",
+  });
 
-    if (!res.ok) {
-        throw new Error('Failed to refresh access token');
-    }
+  if (!res.ok) {
+    throw new Error("Failed to refresh access token");
+  }
 
-    const data = await res.json();
-    return data;
+  const data = await res.json();
+  return data;
 }
