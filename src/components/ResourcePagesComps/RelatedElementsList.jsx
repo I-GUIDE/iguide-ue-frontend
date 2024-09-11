6e8148a77adbcf69601c84dd0478f2bebabc83f2
Erick Li <ericklizh@gmail.com>
2024-09-11 10:57:50 -0500

    Use carousel to display related element list

--- src/components/ResourcePagesComps/RelatedElementsList.jsx
+++ src/components/ResourcePagesComps/RelatedElementsList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Carousel from "react-multi-carousel";
 
 import Divider from "@mui/joy/Divider";
 import Typography from "@mui/joy/Typography";
 import Stack from "@mui/joy/Stack";
 import Box from "@mui/joy/Box";
-import Grid from "@mui/joy/Grid";
 
 import SimpleInfoCard from "../SimpleInfoCard";
+
+import "react-multi-carousel/lib/styles.css";
 
 export default function RelatedElementsList(props) {
   const title = props.title;
@@ -20,38 +22,54 @@ export default function RelatedElementsList(props) {
     return null;
   }
 
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 5000, min: 1200 },
+      items: 3,
+    },
+    desktopNarrow: {
+      breakpoint: { max: 1200, min: 900 },
+      items: 2,
+    },
+    tablet: {
+      breakpoint: { max: 900, min: 650 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 650, min: 400 },
+      items: 2,
+    },
+    mobileSmall: {
+      breakpoint: { max: 400, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <Stack spacing={2} sx={{ px: { xs: 2, md: 4 }, py: 3 }}>
       <Typography level="h5" fontWeight="lg" mb={1}>
         {title}
       </Typography>
       <Divider inset="none" />
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          {relatedElements?.map((relatedElement) => (
-            <Grid
-              key={relatedElement.id}
-              xs={12}
-              sm={6}
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              minHeight={180}
-              sx={{ p: 3 }}
-            >
-              <SimpleInfoCard
-                key={relatedElement.id}
-                cardtype={relatedElement["resource-type"] + "s"}
-                pageId={relatedElement.id}
-                title={relatedElement.title}
-                thumbnailImage={relatedElement["thumbnail-image"]}
-                minHeight="100%"
-                width="100%"
-              />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      <Carousel responsive={responsive}>
+        {relatedElements?.map((relatedElement) => (
+          <Box
+            key={relatedElement.id}
+            sx={{ p: 0.5 }}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <SimpleInfoCard
+              cardtype={relatedElement["resource-type"] + "s"}
+              pageId={relatedElement.id}
+              title={relatedElement.title}
+              thumbnailImage={relatedElement["thumbnail-image"]}
+              minHeight="100%"
+              width="100%"
+            />
+          </Box>
+        ))}
+      </Carousel>
     </Stack>
   );
 }
