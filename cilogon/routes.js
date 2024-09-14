813b0ffc4ddb66d44a349d077ab98b4881924147
Erick Li <ericklizh@gmail.com>
2024-09-13 21:44:08 -0500

    Update untrusted_user role number

--- cilogon/routes.js
+++ cilogon/routes.js
@@ -115,7 +115,7 @@ router.get('/cilogon-callback', async (req, res, next) => {
       }
 
       // Retrieve user role from OpenSearch
-      const role = await getUserRole(user.sub) || 'untrusted_user'; // Default to 'user' if role not found
+      const role = await getUserRole(user.sub) || 10; // Default to 'user' if role not found
       console.log('user role: ', role);
 
       // Generate JWT token with role
