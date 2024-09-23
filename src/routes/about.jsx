48fe8fc7af234a09e0965cf5a27c97f14b705ff9
Erick Li <ericklizh@gmail.com>
2024-09-23 17:44:28 -0500

    Update About page bolierplate

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { Link as RouterLink } from "react-router-dom";
 
 import { CssVarsProvider } from "@mui/joy/styles";
 import CssBaseline from "@mui/joy/CssBaseline";
 import Box from "@mui/joy/Box";
-import Container from "@mui/joy/Container";
 import Grid from "@mui/joy/Grid";
-
-import usePageTitle from "../hooks/usePageTitle";
+import Stack from "@mui/joy/Stack";
+import Container from "@mui/joy/Container";
+import Divider from "@mui/joy/Divider";
+import Link from "@mui/joy/Link";
+import Typography from "@mui/joy/Typography";
 
 import { NO_HEADER_BODY_HEIGHT } from "../configs/VarConfigs";
+import usePageTitle from "../hooks/usePageTitle";
 
 export default function About() {
   usePageTitle("About");
@@ -16,7 +21,7 @@ export default function About() {
   return (
     <CssVarsProvider disableTransitionOnChange>
       <CssBaseline />
-      <Container maxWidth="lg">
+      <Container maxWidth="md">
         <Box
           component="main"
           sx={{
@@ -28,19 +33,30 @@ export default function About() {
         >
           <Grid
             container
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            direction="column"
+            rowSpacing={2}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
             sx={{
-              minHeight: NO_HEADER_BODY_HEIGHT,
               backgroundColor: "inherit",
               px: { xs: 2, md: 4 },
               pt: 4,
               pb: 8,
             }}
           >
-            Future "About" page
+            <Grid xs={12}>
+              <Stack
+                spacing={3}
+                alignItems={{ xs: "flex-start", md: "center" }}
+                sx={{ p: 2 }}
+              >
+                <Typography level="h2">About I-GUIDE Platform</Typography>
+              </Stack>
+
+              <Divider sx={{ mx: 2, my: 4 }} />
+
+              <Typography level="body-md" sx={{ p: 2 }}>
+                Future "About" contents
+              </Typography>
+            </Grid>
           </Grid>
         </Box>
       </Container>
