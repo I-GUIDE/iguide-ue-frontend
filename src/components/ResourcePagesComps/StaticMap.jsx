7324e3caf09cc98f3855d2977caa1fecb4939938
Erick Li <ericklizh@gmail.com>
2024-10-10 11:56:42 -0500

    Add a link to view static map in new window

--- src/components/ResourcePagesComps/StaticMap.jsx
+++ src/components/ResourcePagesComps/StaticMap.jsx
@@ -5,6 +5,7 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import Stack from "@mui/joy/Stack";
 import Box from "@mui/joy/Box";
 import Typography from "@mui/joy/Typography";
+import Link from "@mui/joy/Link";
 
 export default function StaticMap(props) {
   const mapImg = props.mapImg;
@@ -17,6 +18,13 @@ export default function StaticMap(props) {
     <Stack spacing={2} sx={{ px: { xs: 2, md: 4 }, py: 3 }}>
       <Typography level="h5" fontWeight="lg" mb={1}>
         Map (Panable)
+      </Typography>
+      <Typography color="neutral" level="body-xs" variant="plain">
+        Click{" "}
+        <Link href={mapImg} target="_blank" rel="noopener noreferrer">
+          here
+        </Link>{" "}
+        to view the static map in new window.
       </Typography>
       <Box
         sx={{
