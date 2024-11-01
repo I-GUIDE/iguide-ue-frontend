f6d081bdeb121ba5490518dfa753f114359170a1
Erick Li <ericklizh@gmail.com>
2024-11-01 12:54:44 -0500

    Add an image upload button

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -26,49 +26,73 @@ const VisuallyHiddenInput = styled("input")`
   width: 1px;
 `;
 
-const buttons = [
-  "undo",
-  "redo",
-  "|",
-  "bold",
-  "underline",
-  "italic",
-  "strikethrough",
-  "|",
-  "superscript",
-  "subscript",
-  "|",
-  "align",
-  "|",
-  "ul",
-  "ol",
-  "outdent",
-  "indent",
-  "|",
-  "font",
-  "fontsize",
-  "brush",
-  "paragraph",
-  "|",
-  "image",
-  "link",
-  "table",
-  "|",
-  "hr",
-  "eraser",
-  "copyformat",
-  "|",
-  "fullsize",
-  "|",
-  "source",
-  "|",
-];
-
 export default function MarkdownEditor(props) {
   const editor = useRef(null);
 
   const contents = props.contents;
   const setContents = props.setContents;
+
+  const [copied, setCopied] = useState(false);
+  const [imgMarkdown, setImgMarkdown] = useState();
+  const [uploadSucceeded, setUploadSucceeded] = useState(false);
+
+  function imageUpload(editor) {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+
+    input.onchange = async function () {
+      const toBeUploaded = input.files[0];
+
+      TEST_MODE && console.log("image to be uploaded", toBeUploaded);
+
+      if (!toBeUploaded.type.startsWith("image/")) {
+        alert("Please upload an image!");
+        return null;
+      }
+      if (toBeUploaded.size > IMAGE_SIZE_LIMIT) {
+        alert("Please upload an image smaller than 5MB!");
+        return null;
+      }
+      const imgFile = toBeUploaded;
+      const imgURL = URL.createObjectURL(toBeUploaded);
+
+      TEST_MODE && console.log("Image info", imgFile, imgURL);
+      setCopied(false);
+
+      // If user uploads a new thumbnail, use the new one, otherwise, use the existing one.
+      if (imgFile) {
+        const formData = new FormData();
+        formData.append("file", imgFile);
+
+        try {
+          const response = await fetchWithAuth(
+            `${USER_BACKEND_URL}/api/elements/thumbnail`,
+            {
+              method: "POST",
+              body: formData,
+            }
+          );
+          TEST_MODE && console.log("Response", formData, response);
+
+          const result = await response.json();
+
+          setImgMarkdown("![image](" + result.url + ")");
+          setUploadSucceeded(true);
+          TEST_MODE && console.log("Img link", result.url);
+          this.insertImage(editor, result.url);
+        } catch (error) {
+          console.error("Error fetching a single element: ", error.message);
+          setImgMarkdown("WARNING: Upload failed...");
+          setUploadSucceeded(false);
+        }
+      } else {
+        setImgMarkdown("WARNING: No file to be uploaded...");
+        setUploadSucceeded(false);
+      }
+    }.bind(this);
+  }
 
   const config = useMemo(
     () => ({
@@ -77,15 +101,58 @@ export default function MarkdownEditor(props) {
       uploader: {
         insertImageAsBase64URI: true,
       },
-      buttons: buttons,
+      buttons: [
+        "undo",
+        "redo",
+        "|",
+        "bold",
+        "underline",
+        "italic",
+        "strikethrough",
+        "|",
+        "superscript",
+        "subscript",
+        "|",
+        "align",
+        "|",
+        "ul",
+        "ol",
+        "outdent",
+        "indent",
+        "|",
+        "font",
+        "fontsize",
+        "brush",
+        "paragraph",
+        "|",
+        "image",
+        "link",
+        "table",
+        "|",
+        "hr",
+        "eraser",
+        "copyformat",
+        "|",
+        "fullsize",
+        "|",
+        "source",
+        "|",
+      ],
+      extraButtons: [
+        {
+          name: "uploadImage",
+          tooltip: "Insert an image",
+          iconURL:
+            "https://www.kindpng.com/picc/m/261-2619141_cage-clipart-victorian-cloud-upload-icon-svg-hd.png",
+          exec: async (editor) => {
+            imageUpload(editor);
+          },
+        },
+      ],
       height: 800,
     }),
     []
   );
-
-  const [copied, setCopied] = useState(false);
-  const [imgMarkdown, setImgMarkdown] = useState();
-  const [uploadSucceeded, setUploadSucceeded] = useState(false);
 
   function handleCopy() {
     setCopied(true);
