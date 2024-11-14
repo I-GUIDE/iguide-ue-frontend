dd6d1b13c1e3eea8087c50b2458f31d89b4171b9
Erick Li <ericklizh@gmail.com>
2024-11-14 11:46:47 -0600

    Use ElementDeleteButton in ContributorOps

--- src/features/Element/ContributorOps.jsx
+++ src/features/Element/ContributorOps.jsx
@@ -1,27 +1,16 @@
-import { React, useState } from "react";
+import * as React from "react";
 
 import { useOutletContext, Link as RouterLink } from "react-router-dom";
 
 import Stack from "@mui/joy/Stack";
 import Tooltip from "@mui/joy/Tooltip";
-import DialogTitle from "@mui/joy/DialogTitle";
-import DialogContent from "@mui/joy/DialogContent";
-import DialogActions from "@mui/joy/DialogActions";
-import Modal from "@mui/joy/Modal";
-import ModalDialog from "@mui/joy/ModalDialog";
-import WarningRoundedIcon from "@mui/icons-material/WarningRounded";
-import Divider from "@mui/joy/Divider";
-import Button from "@mui/joy/Button";
 import Link from "@mui/joy/Link";
 import IconButton from "@mui/joy/IconButton";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
-import { fetchWithAuth } from "../../utils/FetcherWithJWT";
 import { PERMISSIONS } from "../../configs/Permissions";
-
-const USER_BACKEND_URL = import.meta.env.VITE_DATABASE_BACKEND_URL;
-const TEST_MODE = import.meta.env.VITE_TEST_MODE;
+import ElementDeleteButton from "../../components/ElementDeleteButton";
 
 export default function ContributorOps(props) {
   const elementId = props.elementId;
@@ -32,8 +21,6 @@ export default function ContributorOps(props) {
     : "/";
   const isPrivateElement = props.isPrivateElement;
 
-  const [deleteMetadataTitle, setDeleteMetadataTitle] = useState(undefined);
-  const [deleteMetadataId, setDeleteMetadataId] = useState(undefined);
   const updateFormUri = `/element-update/${elementId}${
     isPrivateElement ? "?private-mode=true" : ""
   }`;
@@ -52,34 +39,6 @@ export default function ContributorOps(props) {
     return null;
   }
 
-  async function handleElementDelete(elementId) {
-    TEST_MODE && console.log("Deleting...", elementId);
-    try {
-      const response = await fetchWithAuth(
-        `${USER_BACKEND_URL}/api/elements/${elementId}`,
-        {
-          method: "DELETE",
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Error deleting resource");
-      }
-
-      setDeleteMetadataId(undefined);
-      setDeleteMetadataTitle(undefined);
-
-      const result = await response.json();
-      // When the deletion was successful, rerender the list
-      if (result && result.message === "Resource deleted successfully") {
-        window.location.href = afterDeleteRedirection;
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      alert("Error deleting resource");
-    }
-  }
-
   return (
     <Stack direction="row" spacing={1} sx={{ px: { xs: 2, md: 4 }, py: 1 }}>
       <IconButton size="sm" variant="soft" color="primary">
@@ -95,62 +54,17 @@ export default function ContributorOps(props) {
           </Tooltip>
         </Link>
       </IconButton>
-      <IconButton
-        aria-label="Delete this element"
+      <ElementDeleteButton
         color="danger"
         variant="soft"
         size="sm"
-        onClick={() => {
-          setDeleteMetadataTitle(title);
-          setDeleteMetadataId(elementId);
-          TEST_MODE && console.log("Attempting to delete:", title, elementId);
-        }}
+        title={title}
+        elementId={elementId}
+        contributorId={contributorId}
+        afterDeleteRedirection={afterDeleteRedirection}
       >
-        <Tooltip title="Delete this element" placement="top" arrow>
-          <DeleteForeverIcon />
-        </Tooltip>
-      </IconButton>
-      <Modal
-        open={!!deleteMetadataTitle && !!deleteMetadataId}
-        onClose={() => {
-          setDeleteMetadataId(undefined);
-          setDeleteMetadataTitle(undefined);
-        }}
-      >
-        <ModalDialog variant="outlined" role="alertdialog" color="danger">
-          <DialogTitle>
-            <WarningRoundedIcon />
-            Confirmation
-          </DialogTitle>
-          <Divider />
-          <DialogContent>
-            Are you sure you want to delete "{deleteMetadataTitle}"? This
-            deletion is permanent!
-          </DialogContent>
-          <DialogActions>
-            <Button
-              variant="solid"
-              color="danger"
-              onClick={() => handleElementDelete(deleteMetadataId)}
-            >
-              Delete
-              {!isContributor &&
-                canEditAllElements &&
-                " (YOU ARE NOT THE CONTRIBUTOR)"}
-            </Button>
-            <Button
-              variant="plain"
-              color="neutral"
-              onClick={() => {
-                setDeleteMetadataId(undefined);
-                setDeleteMetadataTitle(undefined);
-              }}
-            >
-              Cancel
-            </Button>
-          </DialogActions>
-        </ModalDialog>
-      </Modal>
+        <DeleteForeverIcon />
+      </ElementDeleteButton>
     </Stack>
   );
 }
