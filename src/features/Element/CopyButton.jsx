aa809763ba937f72bf4c47fa30122b7145dd5a18
Erick Li <ericklizh@gmail.com>
2024-11-22 16:52:55 -0600

    Add a snackbar for successful copy

--- src/features/Element/CopyButton.jsx
+++ src/features/Element/CopyButton.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Tooltip from "@mui/joy/Tooltip";
 import Box from "@mui/joy/Box";
 import IconButton from "@mui/joy/IconButton";
+import Snackbar from "@mui/joy/Snackbar";
+import Typography from "@mui/joy/Typography";
 
 import LinkIcon from "@mui/icons-material/Link";
 
@@ -12,22 +14,27 @@ export default function CopyButton(props) {
   const textToCopy = props.textToCopy;
   TEST_MODE && console.log("text to copy", textToCopy);
 
+  const [copied, setCopied] = useState(false);
+
   if (!textToCopy) {
     return null;
   }
 
   async function copyText(textToCopy) {
     try {
+      // This function only works when the server is under HTTPS
       await navigator.clipboard.writeText(textToCopy);
       TEST_MODE && console.log("Content copied to clipboard", textToCopy);
+      setCopied(true);
     } catch (err) {
       TEST_MODE && console.error("Failed to copy: ", err);
+      setCopied(false);
     }
   }
 
   return (
     <Box sx={{ display: "flex", alignItems: "flex-start", py: 1 }}>
-      <Tooltip title="Copy the link">
+      <Tooltip title="Copy element URL">
         <IconButton
           variant="outlined"
           size="sm"
@@ -36,6 +43,18 @@ export default function CopyButton(props) {
           <LinkIcon />
         </IconButton>
       </Tooltip>
+      <Snackbar
+        autoHideDuration={3000}
+        open={copied}
+        variant="soft"
+        color="primary"
+        size="md"
+        onClose={() => setCopied(false)}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        sx={{ maxWidth: 360 }}
+      >
+        <Typography level="body-md">Element URL copied!</Typography>
+      </Snackbar>
     </Box>
   );
 }
