345399a14507117873516a8fa45d8af4d401ae2c
RC2408 <mailrohancolaco@gmail.com>
2024-12-10 15:50:36 -0600

    Enable page load performance tracking

--- src/utils/Analytics.jsx
+++ src/utils/Analytics.jsx
@@ -1,14 +1,34 @@
-import React from "react";
-import ReactGA from "react-ga4";
+import React, { useEffect } from "react";
+import ReactGA4 from "react-ga4";
 
 const VITE_GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
 
 export default function Analytics(props) {
-  ReactGA.initialize([
+  ReactGA4.initialize([
     {
       trackingId: VITE_GA_MEASUREMENT_ID,
     },
   ]);
+
+  useEffect(() => {
+    const observer = new PerformanceObserver((entryList) => {
+      const entries = entryList.getEntries();
+      entries.forEach((entry) => {
+        console.log(entry);
+        ReactGA4.event("Page Performance", {
+          page_performance: entry.duration,
+          label: window.location.pathname,
+          category: "Page performance",
+        });
+      });
+    });
+
+    observer.observe({ entryTypes: ["navigation"] });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return <>{props.children}</>;
 }
