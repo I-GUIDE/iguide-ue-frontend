02efbf821ad8ae3430a9d47eac1418415b333be2
Erick Li <ericklizh@gmail.com>
2025-02-22 16:56:32 -0600

    Check user role from JWT before entering the submission form

--- src/configs/Permissions.jsx
+++ src/configs/Permissions.jsx
@@ -1,6 +1,7 @@
 // The number means the highest user role number (included) that is
 //  allowed to perform indicated tasks
 export const PERMISSIONS = {
+  secure_default_role: 100,
   default_user: 10,
   contribute: 8,
   edit_all: 3,
