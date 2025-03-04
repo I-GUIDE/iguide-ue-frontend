24df105edbe6c37588f8deccad52643d110b7ce7
Erick Li <ericklizh@gmail.com>
2025-03-04 14:35:49 -0600

    Change env variable VITE_SEND_ERR_TO_SLACK to VITE_DO_NOT_SEND_ERR_TO_SLACK

--- src/utils/AutomaticBugReporting.jsx
+++ src/utils/AutomaticBugReporting.jsx
@@ -1,11 +1,11 @@
 const VITE_SLACK_API_URL = import.meta.env.VITE_SLACK_API_URL;
-const VITE_SEND_ERR_TO_SLACK =
-  import.meta.env.VITE_SEND_ERR_TO_SLACK === "true";
+const VITE_DO_NOT_SEND_ERR_TO_SLACK =
+  import.meta.env.VITE_DO_NOT_SEND_ERR_TO_SLACK === "true";
 const TEST_MODE = import.meta.env.VITE_TEST_MODE;
 
 // Function that sends "message" to the defined Slack channel
 export async function sendMessageToSlack(message) {
-  if (!VITE_SEND_ERR_TO_SLACK) {
+  if (VITE_DO_NOT_SEND_ERR_TO_SLACK) {
     TEST_MODE &&
       console.log("Sending errors to Slack disabled... Message:", message);
     return;
