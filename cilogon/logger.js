18c262c53a5982220d8c89bb42ee0ca2debb5081
Erick Li <ericklizh@gmail.com>
2025-03-13 11:06:09 -0500

    Output logs to a folder outside of the container for retention

--- cilogon/logger.js
+++ cilogon/logger.js
@@ -2,17 +2,38 @@
 const pino = require('pino');
 const pinoHttp = require('pino-http');
 
+// Get current timestamp for the logging structure
+const currentTime = new Date();
+const yearUTC = String(currentTime.getUTCFullYear()).padStart(4, '0');
+const monthUTC = String(currentTime.getUTCMonth() + 1).padStart(2, '0');
+const dayUTC = String(currentTime.getUTCDate()).padStart(2, '0');
+
 const logger = pino({
   level: process.env.LOGGER_LOG_LEVEL || "info",
   transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: 'UTC:yyyy-mm-dd HH:MM:ss.l o'
-    }
+    targets: [
+      // Channel the log to a destination file
+      {
+        target: 'pino/file',
+        options: {
+          // Structure: logs/yyyy/mm/auth-{yyyy-mm-dd}.log
+          destination: `/app/logs/${yearUTC}/${monthUTC}/auth-${yearUTC}-${monthUTC}-${dayUTC}.log`,
+          mkdir: true
+        }
+      },
+      // Prettify the log for STDOUT
+      {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: 'UTC:yyyy-mm-dd HH:MM:ss.l o'
+        }
+      }
+    ]
   },
+  // Redact sensitive information and PII
   redact: ["user.email", "user.given_name"],
-}, pino.destination("./auth-server.log"));
+});
 
 const httpLogger = pinoHttp({ logger });
 
