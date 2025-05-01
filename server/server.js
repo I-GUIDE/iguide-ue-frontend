8aa6fd82108b86d75dda5c7eca7a0e787e57f9a8
RC2408 <mailrohancolaco@gmail.com>
2025-05-01 14:18:31 -0500

    Configure ports on docker

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const prerender = require("prerender");
-const server = prerender({
-  chromeLocation: "/usr/bin/google-chrome-stable",
-  chromeFlags: [
-    "--no-sandbox",
-    "--headless",
-    "--disable-gpu",
-    "--remote-debugging-port=9222",
-  ],
-});
+const server = prerender(
+  {
+    chromeLocation: "/usr/bin/google-chrome-stable",
+    chromeFlags: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--headless",
+      "--disable-gpu",
+      "--remote-debugging-port=9222",
+      // "--remote-debugging-address=0.0.0.0",
+      "--hide-scrollbars",
+      "--disable-dev-shm-usage",
+    ],
+  },
+  { shell: true }
+);
+
 server.start();
