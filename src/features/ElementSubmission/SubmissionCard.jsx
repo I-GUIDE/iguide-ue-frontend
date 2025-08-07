471c5d74d38eaa69828d7c73ae1d2d417f1f16ee
Erick Li <ericklizh@gmail.com>
2025-08-07 10:31:20 -0500

    Fix an issue where the dataset information doesn't get included in the submission formdata

--- src/features/ElementSubmission/SubmissionCard.jsx
+++ src/features/ElementSubmission/SubmissionCard.jsx
@@ -1106,6 +1106,12 @@ export default function SubmissionCard(props) {
     data["contents"] = contents;
     data["external-link-publication"] = publicationDOI;
 
+    if (resourceTypeSelected === "dataset") {
+      data["external-link"] = datasetExternalLink;
+      data["direct-download-link"] = directDownloadLink;
+      data["size"] = dataSize;
+    }
+
     if (resourceTypeSelected === "oer") {
       data["oer-external-links"] = oerExternalLinks;
     }
