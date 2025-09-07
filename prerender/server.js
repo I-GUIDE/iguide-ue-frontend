3f92b119e719a3199705a0a22916e8bc5a2b1103
rohan <rohan@upsure.io>
2025-09-06 21:01:35 -0500

    Implement prerender using browserless/chrome docker image

---
+++ prerender/server.js
@@ -0,0 +1,16 @@
+const prerender = require("prerender");
+
+const server = prerender({
+  followRedirects: true,
+  chromeLocation: "/usr/bin/google-chrome",
+  chromeFlags: [
+    "--no-sandbox",
+    "--headless",
+    "--disable-gpu",
+    "--remote-debugging-port=9222",
+    "--hide-scrollbars",
+  ],
+  port: 8000,
+});
+server.use(require("prerender-memory-cache"));
+server.start();
